4200f52d8d841fd5da50b79ab67244cf73e3377d
Halfdan Sand Christensen <halfdansc@proton.me>
2025-11-25 21:42:26 +0100

    variables

---
+++ first_take.lua
@@ -0,0 +1,20 @@
+-- This is a comment in Lua.
+
+
+--[[ This is also
+	a comment.
+
+	but it spans multiple lines!
+--]]
+
+-- local
+local number = 5
+local string = "hello world"
+local single 'also a string'
+local crazy = [[ multi
+	line literal ]]
+
+local truth, lies = true, false
+local nothing = nil
+
+
